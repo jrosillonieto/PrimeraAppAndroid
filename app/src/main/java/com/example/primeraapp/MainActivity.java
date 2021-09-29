123ad43dd6dd0c0c448aae7dc2ffed47f16bb3d0
Jose <jrosillonieto@gmail.com>
2021-09-29 19:59:37 +0200

    app prova

---
+++ app/src/main/java/com/example/primeraapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.primeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.detonate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "BOUM!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+    }
+}
